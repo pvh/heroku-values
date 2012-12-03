d2a32b9b7853d8c4b1d46f81177a4ef1aba5dd91
Maciek Sakrejda <maciek@heroku.com>
2012-12-02 18:54:19 -0800

    Add some basic caching.

--- init.rb
+++ init.rb
@@ -7,21 +7,36 @@ class Heroku::Command::Values < Heroku::Command::Base
   # output a random value
   #
   def index
-    puts get('http://values-api.herokuapp.com/random')['result']
+    puts get_values.sample
   end
 
   # output all values
   #
   def all
-    get('http://values-api.herokuapp.com/all')['result'].each { |item|
+    get_values.each { |item|
       puts item
     }
   end
 
   private
 
-  def get(path)
-    json_decode(RestClient::Resource.new(path).get.body)
+  def cache_path
+    "#{home_directory}/.heroku/values_cache"
+  end
+
+  def has_cached?
+    File.exists(cache_path) && File.mtime(cache_path) > Time.now - (60 * 24 * 24)
+  end
+
+  def get_values
+    if has_cached?
+      result = File.read(cache_path)
+    else
+      result = RestClient::Resource.new('http://values-api.herokuapp.com/all').get.body
+      File.write(cache_path, result)
+    end
+
+    json_decode(result)['result']
   end
 
 end
